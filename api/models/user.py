aeb0368e7813d690bb795e649169a043a0fc3f3e
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-04-11 19:42:25 +0200

    T-55-Setup de la vista del perfil

--- api/models/user.py
+++ api/models/user.py
@@ -5,11 +5,16 @@ from django.contrib.auth.hashers import make_password, check_password
 class User(models.Model):
     # Podem definir el maxim de caracters de username
     username = models.CharField(max_length=32, primary_key=True)
+    description = models.TextField()
+    cover = models.ImageField()
+    avatar = models.ImageField()
+
+    # TODO: s'ha de veure quins camps respecte el registre s'han de tenir donat que es fa per google
     email = models.EmailField(unique=True)
     # Es pot definir com volem que sigui la contrasenya, quins caràcters especials
     password = models.CharField(max_length=16)
     # Amb auto_now_add es registrara la data i hora en el mateix moemtn que es crei el registre
-  #  register_date = models.DateTimeField(auto_now_add=True)
+    #  register_date = models.DateTimeField(auto_now_add=True)
 
     def set_password(self, password):
         # Guarda la contrasenya de forma segura amb bcrypt
