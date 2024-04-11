ad3f800e7b7e8eb137b677f7bea5a8f3b7e5d441
alex.brichs <alex.brichs@estudiantat.upc.edu>
2024-04-11 20:51:22 +0200

    <tasca 66> AfegirComentari

--- api/models/comment.py
+++ api/models/comment.py
@@ -1,15 +1,19 @@
 from django.db import models
 from .user import User
 from django.utils import timezone
+from .thread import Thread
 
 
-class Thread(models.Model):
+class Comment(models.Model):
     # Automaticament, si no s'especifica, django genera un id (int) com a PK
-    # Rang dels likes [0-2147483647]
-    num_likes = models.PositiveIntegerField(default=0)
+
     # En cas que l'author associat s'elimini, tots el threads seus s'elimines
     author = models.ForeignKey(User, on_delete=models.CASCADE, default='default_user')
-    title = models.TextField(max_length=255, default='')
     body = models.TextField(max_length=35000, null=True)
-    url = models.TextField(max_length=35000, default='')
     creation_data = models.DateTimeField(default=timezone.now)
+    # Rang dels likes [0-2147483647]
+    num_likes = models.PositiveIntegerField(default=0)
+    num_dislikes = models.PositiveIntegerField(default=0)
+
+    # En cas que el thread associat s'elimini, tots els seus comentaris s'eliminen
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE, default='default_thread')
--- projecte_asw/settings.py
+++ projecte_asw/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -27,7 +27,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'api',
     'rest_framework'
 ]
@@ -71,7 +71,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'projecte_asw.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
@@ -81,7 +80,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
@@ -101,7 +99,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
@@ -113,12 +110,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (css, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-#STATIC_URL = 'static/'
-#STATIC_ROOT = ''
+# STATIC_URL = 'static/'
+# STATIC_ROOT = ''
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = ('static',)
