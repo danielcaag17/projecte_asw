aa9620d671e09eacd5b656f67d7f10c1fd758af0
alex.brichs <alex.brichs@estudiantat.upc.edu>
2024-04-15 19:12:34 +0200

    Preparant el deploy a Heroku v5

--- projecte_asw/settings.py
+++ projecte_asw/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -94,13 +95,21 @@ WSGI_APPLICATION = 'projecte_asw.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# Obtener la URL de la base de datos de la variable de entorno
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
+# Configurar las bases de datos
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
