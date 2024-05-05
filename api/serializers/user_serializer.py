eec8767840698305e0474715c7a74f14dec58932
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-05-05 16:27:06 +0200

    T-106 Veure info usuari(s)

--- api/serializers/user_serializer.py
+++ api/serializers/user_serializer.py
@@ -3,6 +3,21 @@ from kbin.models.user import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+    total_threads = serializers.SerializerMethodField()
+    total_comments = serializers.SerializerMethodField()
+    total_boosts = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = ['username', 'description', 'cover', 'avatar', 'email']
+        fields = ['username', 'description', 'cover', 'avatar', 'email',
+                  'total_threads', 'total_comments', 'total_boosts']
+
+    def get_total_threads(self, obj):
+        return obj.total_threads
+
+    def get_total_comments(self, obj):
+        return obj.total_comments
+
+    def get_total_boosts(self, obj):
+        return obj.total_boosts
+
