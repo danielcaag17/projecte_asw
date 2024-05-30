e8a30ad4f304b846c397426607f38de0034cd9fd
Joan <joan.marinoso@estudiantat.upc.edu>
2024-05-30 20:22:47 +0200

    Fix serializer vots comentaris

--- api/serializers/serializer_vots_comentaris.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from kbin.models.vote_comment import *
-
-class VoteCommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vote_comment
-        fields = ['comment_id','type']
